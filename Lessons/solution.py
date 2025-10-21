bbf9f2dc0fce662692911ea85edfd012c3cae5cb
yasso tsd <tajbouk@gmail.com>
2025-10-21 01:44:01 +0100

    Create solution.py

---
+++ Lessons/solution.py
@@ -0,0 +1,9 @@
+# solution.py
+
+print("Hello TON from Termux!")
+
+# Simulate TON interaction (placeholder)
+def ton_simulation():
+    print("Simulating TON smart contract call...")
+
+ton_simulation()
